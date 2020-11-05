7f88251317413c31bd922d5ed980d754581efaf6
Rameesa <ramikp@gmail.com>
2020-11-05 15:06:12 +0530

    second

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lms*gmb$j4u81w7wo2kou!2oat!jgk7x^*ujs^3_h7-klx%)hs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
